f18a7202377f314b1fee9228851c6c82e4ae1c86
bmacindo <bmacindo@ciso.com>
2023-04-12 09:10:59 +0000

    initial commit

---
+++ sample_app.py
@@ -0,0 +1,15 @@
+# Add to this file for the sample app lab
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+    
